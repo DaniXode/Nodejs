66f793f3605e73f2c849b8ec009e7a98d9600a8a
DaniXode <danieldemerw777@gmail.com>
2024-01-21 02:27:30 -0500

    first server

---
+++ 01-server.js
@@ -0,0 +1,7 @@
+const http =require('http')
+const port =process.env.PORT ||1337
+const server =http.creatServer(function(req,res){
+	res.end('hi')
+})
+server.listen(port)
+console.log(`server listening on port${port}`)
